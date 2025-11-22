21202ab3742b2913c4c578a17574a369e2aaf9d7
konat1993 <konat1993@gmail.com>
2025-11-22 19:43:02 +0100

    macos-6: added safari window

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Dock, Navbar, Welcome } from "#components";
-import { TerminalWindow } from "#windows";
+import { SafariWindow, TerminalWindow } from "#windows";
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
@@ -13,6 +13,7 @@ const App = () => {
 			<Dock />
 
 			<TerminalWindow />
+			<SafariWindow />
 		</main>
 	);
 };
